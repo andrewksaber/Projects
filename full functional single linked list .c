b67659b34821eea990111e5bd418b97c939500cc
andrewksaber <109389033+andrewksaber@users.noreply.github.com>
2022-07-17 14:15:33 +0200

    Update full functional single linked list .c

--- full functional single linked list .c
+++ full functional single linked list .c
@@ -58,6 +58,32 @@ return head;
 
 
 };
+struct node * bsort(struct node *head ){
+struct node * ptr = malloc(sizeof(struct node)) ;
+ptr = head ;
+int size = list_size(head) ;
+int arr[size] ;
+for(int i  =0 ; i<size ; i++){
+    arr[i] = ptr->data ;
+    ptr = ptr->link ;
+}
+for(int i = 0 ; i<size-1 ;i++){
+    for(int j =i+1 ; j<size ;j++){
+        if(arr[i]>arr[j]){
+        int temp = arr[i] ;
+        arr[i] = arr[j] ;
+        arr[j] = temp ;
+    }
+}
+}
+ptr = head;
+for(int i  =0 ; i<size ; i++){
+   ptr->data  =arr[i];
+    ptr = ptr->link ;
+}
+return head;
+};
+
 
 void del_particular(int pos , struct node *head) {
 struct node * ptr = head  ;
