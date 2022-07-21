42b2ed6f50b03e2f78b2b55e0070f1db51ffabdb
andrewksaber <109389033+andrewksaber@users.noreply.github.com>
2022-07-21 20:56:00 +0200

    Update full_functional_doubly_list.c

--- full_functional_doubly_list.c
+++ full_functional_doubly_list.c
@@ -2,227 +2,235 @@
 #include <stdlib.h>
 struct node {
 int data ;
-struct node * next ;
-struct node * prev;
+struct node * next  ;
+};
+
+struct node *add_at_empty(int d ){
+struct node * tail = malloc(sizeof(struct node )) ;
+tail->data =d;
+tail->next = tail ;
+return tail ;
 
 };
-struct node * add_at_empty(int d , struct node * head ){
-head->data = d ;
-head->next = NULL ;
-head->prev = NULL;
-return head ;
-}
-struct node * add_at_beg(int d , struct node * head ){
+struct node * add_at_beg(int d , struct node * tail ){
 struct node * temp = malloc(sizeof(struct node)) ;
-temp->data = d;
-temp ->next = head  ;
-head ->prev = temp ;
-head  =temp ;
-
-return head;
+temp->data= d ;
+temp->next = tail->next ;
+tail->next = temp ;
+return tail  ;
 };
-void  add_at_end (int d , struct node * head ){
-struct node * temp = malloc(sizeof(struct node)) ;
-temp->data  =d  ;
-temp->next = NULL ;
-struct node * ptr = head ;
-while(ptr->next!=NULL ){
-    ptr = ptr->next ;
-}
-ptr->next = temp ;
-temp->prev = ptr ;
+struct node * add_at_end(int d , struct node * tail ){
+struct node * temp = malloc(sizeof(struct node )) ;
+temp->data = d ;
+temp->next = tail->next ;
+tail->next = temp ;
+tail = temp ;
+return tail  ;
 
-
-}
-int list_size(struct node * head ){
-int size = 0 ;
-struct node * ptr =head ;
-while(ptr !=NULL){
+};
+int list_size(struct node * tail ){
+struct node * ptr = tail ->next ;
+int size = 0  ;
+do{
     size++;
-    ptr = ptr->next  ;
-}
+ptr = ptr->next ;
+
+}while(ptr!=tail->next) ;
 return size ;
 
-
 }
-struct node * add_at_pos(int d , int pos ,struct node * head ){
-
-if(head == NULL){
-    head = add_at_empty(d,head) ;
-    return head ;
+struct node * add_at_pos(int d , int pos ,struct node * tail ){
+if(pos==1){
+    tail = add_at_beg(d,tail) ;
+    return tail ;
 }
-else if(pos ==1 ){
-    head = add_at_beg(d,head) ;
-    return head ;
+else if(pos==list_size(tail)+1){
+    tail = add_at_end(d,tail);
+    return tail ;
 }
-else if(pos == list_size(head)+1){
-    add_at_end(d,head) ;
-    return head ;
+else if(pos>list_size(tail)+1){
+    printf("\n position doesn't exist ") ;
+    return tail ;
 }
 else {
-struct node * temp = malloc(sizeof(struct node)) ;
-temp->data = d ;
-pos-- ;
-struct node * ptr = head ;
-while(pos!=1){
-
-    ptr = ptr->next ;
-pos-- ;
-}
-temp->next = ptr->next ;
-temp->prev = ptr ;
-ptr ->next = temp ;
-
-return head ;
-
-}
-}
-struct node * reverse_list(struct node * head ){
-struct node * ptr = head ;
-struct node * newhead = malloc(sizeof(struct node))  ;
-newhead = add_at_empty(head->data,newhead) ;
-ptr = ptr->next ;
-while(ptr!=NULL){
-newhead=add_at_beg(ptr->data ,newhead);
-ptr = ptr->next ;
-}
-return newhead ;
-
-
-}
-void add_after(int d , int pos , struct node * head){
-struct node * temp = malloc(sizeof(struct node))  ;
-temp ->data = d;
-
-struct node * ptr =head ;
-while(pos!=1){
-    ptr =ptr->next ;
-    pos--;
-}
-temp ->next =  ptr ->next ;
-temp ->prev = ptr ;
-ptr ->next = temp ;
-
-}
-void add_before(int d , int pos , struct node * head){
-struct node * temp = malloc(sizeof(struct node))  ;
-temp ->data = d;
-
-struct node * ptr =head ;
-pos-=2;
-while(pos!=1){
-
-    ptr =ptr->next ;
-        pos--;
-
-}
-temp ->next =  ptr ->next ;
-temp ->prev = ptr ;
-ptr ->next = temp ;
-
-}
-struct node * del_1st_node (struct node * head ){
-head = head->next ;
-free(head->prev) ;
-head->prev = NULL ;
-return head ;
-
-};
-void del_last_node(struct node * head){
-struct node * ptr = head ;
-while(ptr->next->next!=NULL){
-    ptr = ptr->next ;
-}
-free(ptr->next) ;
-
-ptr->prev = ptr->prev ;
-ptr ->next = NULL ;
-
-
-}
-void del_node(int pos , struct node * head ){
-struct node * ptr = head ;
-
-while(pos!=1){
-    ptr = ptr->next ;
     pos-- ;
+    struct node *ptr  =tail->next ;
+    struct node * temp = malloc(sizeof(struct node)) ;
+    temp->data = d ;
+    while(pos!=1){
+        ptr = ptr->next ;
+        pos--;
+    }
+temp->next = ptr->next ;
+ptr ->next = temp ;
+return tail ;
+
 
 }
-struct node * temp2 = ptr->prev  ;
-temp2 ->next =ptr ->next ;
-
-ptr->next->prev = temp2 ;
-free(ptr);
-
-
-
-}
-
-
-struct node * del_list(struct node * head){
-while(head->next!=NULL){
-    head=head->next ;
-    free(head->prev) ;
-}
-head=NULL ;
-return head ;
 
 };
-struct node * reverse_list2(struct node * head ){
-struct node * newhead =  malloc(sizeof(struct node)) ;
-newhead = add_at_empty(head->data ,newhead) ;
-struct node * ptr = head->next ;
-while(ptr!=NULL){
-    newhead =add_at_beg(ptr->data,newhead) ;
+void show(struct node * tail ){
+if(tail == NULL){
+    printf("\n list is empty " ) ;
+    return ;
+}
 
+else{
+struct node * ptr = tail->next ;
+printf("\n");
+do{
+    printf(" %d ",ptr->data) ;
     ptr=ptr->next ;
+}while(ptr!=tail->next) ;
 
 }
-del_list(head) ;
-return newhead ;
+};
+
+struct node * del_1st_node(struct node * tail ){
+if(tail == NULL){
+    printf("\n list is empty ") ;
+    return tail  ;
+}
+else if (tail ->next ==NULL){
+    free(tail);
+    tail =NULL ;
+    return tail ;
+}
+else {
+struct node * del = tail ->next ;
+tail->next = del->next  ;
+free(del) ;
+return tail ;
+
+}
+};
+
+struct node * del_last_node(struct node * tail ){
+if(tail == NULL){
+    printf("\n list is empty " ) ;
+    return tail ;
+}
+else if (tail ->next ==NULL){
+    free(tail);
+    tail =NULL ;
+    return tail ;
+}
+else {
+struct node *ptr = tail->next  ;
+while(ptr->next!=tail) {
+        ptr =ptr->next ;
+
+}
+ptr->next = tail->next ;
+free(tail);
+tail = ptr ;
+return tail ;
+}
+
+};
+struct node * del_at_pos(int pos ,struct node * tail){
+if(pos==1){
+    tail = del_1st_node(tail) ;
+    return tail ;
+}
+else if(pos==list_size(tail)+1){
+    tail = del_last_node(tail);
+    return tail ;
+}
+else if(pos>list_size(tail)+1){
+    printf("\n position doesn't exist ") ;
+    return tail ;
+}
+else {
+
+    pos -- ;
+struct node * ptr = tail->next;
+while(pos!=1){
+    ptr = ptr->next ;
+    pos --;
+}
+struct node *temp = ptr->next ;
+ptr->next = temp->next ;
+free(temp) ;
+return tail ;
+
+
+}
+
 
 };
 
-struct node *reverse_list3(struct node * head ){
-struct node * ptr1 = head ;
-struct node * ptr2 = head->next ;
-ptr1->next = NULL;
-ptr1->prev = ptr2 ;
-while(ptr2!=NULL){
-    ptr2->prev =ptr2->next  ;
+int search_element(int d , struct node * tail ){
+int index =0 ;
 
-    ptr2->next =  ptr1 ;
-    ptr1 =ptr2 ;
-    ptr2 = ptr2->prev ;
+if(tail->data ==d){
+    index = list_size(tail) -1 ;
+    return index  ;
 
 }
-head = ptr1 ;
-return head ;
+else{
+struct node * ptr = tail->next ;
+while(ptr!=tail ){
+    if(ptr->data == d ){
+        return index ;
+        break ;
+    }
+index++;
+ptr = ptr->next ;
+}
 
+
+}
+printf("\n element doesn't exist ") ;
+return -1 ;
+};
+
+struct node * del_list(struct node * tail  ){
+struct node * ptr  = tail;
+int size = list_size(tail) ;
+for(int i = 0 ; i<size ; i++){
+    ptr = tail ;
+    tail = tail->next ;
+    free(ptr) ;
+}
+tail = NULL ;
+
+return tail ;
 
 };
+
+
+struct node * reverse_list(struct node * tail ) {
+struct node * ptr = tail->next ;
+struct node * newtail = add_at_empty(ptr->data) ;
+ptr = ptr->next ;
+do{
+newtail = add_at_beg(ptr->data ,newtail) ;
+ptr = ptr->next ;
+
+}while(ptr!=tail->next) ;
+
+tail = del_list(tail);
+
+return newtail ;
+
+};
+
+
+
+
+
 int main()
 {
-struct node * head=  malloc(sizeof(struct node)) ;
-head= add_at_empty(1,head) ;
-head =add_at_beg(2,head)  ;
-head =add_at_beg(33,head)  ;
-add_at_end(320,head) ;
-head = add_at_pos(20,1,head) ;
-
-struct node * ptr = head  ;
-while(ptr!=NULL){
-    printf("%d ",ptr->data );
-    ptr=ptr->next ;
-}
-
-printf("\n ");
-head = reverse_list3(head);
-  ptr = head  ;
-while(ptr!=NULL){
-    printf("%d ",ptr->data );
-    ptr=ptr->next ;
-}
-
+struct node * tail = add_at_empty(1) ;
+tail =add_at_beg(25,tail ) ;
+tail =add_at_beg(34,tail ) ;
+tail = add_at_end(66,tail) ;
+tail = add_at_pos(11,list_size(tail),tail) ;
+tail  =add_at_pos(33,2,tail )  ;
+show(tail) ;
+tail  =reverse_list(tail) ;
+show(tail );
     return 0;
 }
+
